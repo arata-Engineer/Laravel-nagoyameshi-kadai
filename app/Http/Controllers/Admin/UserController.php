20a06d1c8b0e461bf705f9b1bb7ebc0c3c2cfee9
arata-Engineer <arataspace1@gmail.com>
2024-11-17 17:35:30 +0900

    会員詳細ページ表示しよう

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(Request $request){
+        
+        $keyword = $request->keyword;
+        if($keyword !== null){
+       
+        $users = User::where('name', 'like', "%{$keyword}%")->orwhere('kana', 'like', "%{$keyword}%")->paginate(15);
+        $total = $users->total();
+        }
+        else{
+            $users = User::paginate(15);
+            $total = $users->total();
+        }
+      
+        return view('admin.users.index', compact('users', 'total','keyword'));
+}
+    
+
+    public function show(User $user){
+        
+
+        return view('admin.users.show', compact('user'));
+    }
+}
