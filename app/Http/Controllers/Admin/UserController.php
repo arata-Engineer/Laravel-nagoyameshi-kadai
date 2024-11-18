5013ddd33a647fe34fe3278ff6f58d14c427e366
arata-Engineer <arataspace1@gmail.com>
2024-11-18 23:11:36 +0900

    会員詳細ページ追加修正6

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -25,9 +25,9 @@ class UserController extends Controller
 }
     
 
-    public function show(User $user){
+    public function show($id){
         
-
+        $user = User::find($id);
         return view('admin.users.show', compact('user'));
     }
 }
